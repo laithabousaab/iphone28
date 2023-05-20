299437e469295d72a095b5793f0d95c77b99dd74
laithabousaab <132582804+laithabousaab@users.noreply.github.com>
2023-05-20 12:34:40 +0300

    Add files via upload
    nice and  beautiful website ..you can change phone and choose the correct color

---
+++ iphone2.js
@@ -0,0 +1,22 @@
+let game = document.getElementById('game');
+let container = document.querySelector('.container');
+
+function phones(rot)
+{
+
+game.src=rot;
+
+
+}
+
+
+function color(x)
+{
+
+   container.style.background=x
+    
+}
+
+
+
+
